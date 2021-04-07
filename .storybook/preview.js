10c2b4a888f07a1e62538e1973e4a8b40e1725a7
Adrian Mowat <adrian.mowat@gmail.com>
2021-04-07 15:23:21 +0100

    Add Task

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,6 @@
+import '../src/index.css';
 
+// Configure Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
